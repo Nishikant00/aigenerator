8a776f512872ed82fdfb2da7be3a9cc31b614f30
Nishikant00 <githubnsr@gmail.com>
2024-09-15 22:44:13 +0530

    Env schema validation

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,10 @@
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
+export default function SignUp() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
